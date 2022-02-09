506a1222025c702cb806f0d7b867ec46680a9faa
tdorey00 <tdorey00@gmail.com>
2022-02-08 19:53:26 -0500

    Cleaning up solution

--- StoryPointEstimatorBlazorApp/Program.cs
+++ StoryPointEstimatorBlazorApp/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using StoryPointEstimatorBlazorApp.Data;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
 
 var app = builder.Build();
 
