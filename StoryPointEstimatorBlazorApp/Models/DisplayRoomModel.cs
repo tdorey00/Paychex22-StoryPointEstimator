a214b840bee26fe27b59448fbbb3edf1eb91e69c
tdorey00 <tdorey00@gmail.com>
2022-02-20 16:42:55 -0500

    Started to create layout and data model

---
+++ StoryPointEstimatorBlazorApp/Models/DisplayRoomModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StoryPointEstimatorBlazorApp.Models
+{
+    public class DisplayRoomModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "Room name is too long.")]
+        [MinLength(2, ErrorMessage = "Room name is too short.")]
+        public string roomName { get; set; }
+        public int roomId { get; set; }
+        //public List<> users { get; set; } //Maybe use a list?
+        public DisplayRoomModel()
+        {
+            roomName = "";
+        }
+    }
+}
