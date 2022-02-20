78267c6473d20af05ab17282ccdc250d06818f90
tdorey00 <tdorey00@gmail.com>
2022-02-20 18:51:19 -0500

    Adding Data Model and finishing Layout

--- StoryPointEstimatorBlazorApp/Models/DisplayRoomModel.cs
+++ StoryPointEstimatorBlazorApp/Models/DisplayRoomModel.cs
@@ -4,14 +4,28 @@ namespace StoryPointEstimatorBlazorApp.Models
     public class DisplayRoomModel
     {
         [Required]
-        [StringLength(30, ErrorMessage = "Room name is too long.")]
+        [StringLength(100, ErrorMessage = "Room name is too long.")]
         [MinLength(2, ErrorMessage = "Room name is too short.")]
         public string roomName { get; set; }
+        [Required]
+        [MaxLength(4, ErrorMessage = "Room code is too long.")] //join room
+        [MinLength(4, ErrorMessage = "Room code is too short.")] //join room
         public int roomId { get; set; }
-        //public List<> users { get; set; } //Maybe use a list?
+        
+        //Potential Fix instead of using two data models, using redundant models where each instance of the user stores all room info
+        [Required]
+        [StringLength(50, ErrorMessage = "Username is too long.")]
+        [MinLength(2, ErrorMessage = "Username is too short.")]
+        public string userName { get; set; }
+        public int userId { get; set; }
+        public bool isAdmin { get; set; }
+        //add vote later
+        
         public DisplayRoomModel()
         {
             roomName = "";
+            userName = "";
+            isAdmin = false;
         }
     }
 }
