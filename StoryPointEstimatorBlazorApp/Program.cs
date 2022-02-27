c334617414ab8080869ff99f8e2382b1b37dd5a9
pmill99 <mill2345@fredonia.edu>
2022-02-27 16:05:00 -0500

    MudBlazor installed and set up

--- StoryPointEstimatorBlazorApp/Program.cs
+++ StoryPointEstimatorBlazorApp/Program.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
