c50989c60d891ae71e1e53c241c93654d63ec0a5
tdorey00 <tdorey00@gmail.com>
2022-03-06 15:22:56 -0500

    FRED-172 and FRED-177 and FRED-180 fixed

--- StoryPointEstimatorBlazorApp/Models/DisplayGroupedModel.cs
+++ StoryPointEstimatorBlazorApp/Models/DisplayGroupedModel.cs
@@ -5,16 +5,14 @@ namespace StoryPointEstimatorBlazorApp.Models
     {
         [Required]
         [StringLength(100, ErrorMessage = "Room name is too long.")]
-        [MinLength(2, ErrorMessage = "Room name is too short.")]
+        [MinLength(3, ErrorMessage = "Room name is too short.")]
         public string roomName { get; set; } = "";
         [Required]
         [Range(1000, 9999, ErrorMessage = "Room Code invalid (1000-9999).")]
         public int roomId { get; set; }
-
-        //Potential Fix instead of using two data models, using redundant models where each instance of the user stores all room info
         [Required]
         [StringLength(50, ErrorMessage = "Username is too long.")]
-        [MinLength(2, ErrorMessage = "Username is too short.")]
+        [MinLength(3, ErrorMessage = "Username is too short.")]
         public string userName { get; set; } = "";
         public int userId { get; set; }
         public bool isAdmin { get; set; } = false;
