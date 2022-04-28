9dbfe065df3e26e4c445841cfd9bcab38ba05a07
pmill99 <mill2345@fredonia.edu>
2022-04-28 14:16:29 -0400

    Submission validation on join room page is verified

--- Testing/baseTest.cs
+++ Testing/baseTest.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using Xunit;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 
 
 namespace Testing
@@ -14,10 +15,11 @@ namespace Testing
     public class baseTest
     {
         [Fact]
-        public async void createRoomWorkflow()
+        public void userA()
         {
             //Initialize Driver and navigate to site
             var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://paychex-story-point-estimator.azurewebsites.net/");
 
             //Click on create room button and assert that the navigation executed correctly
@@ -26,7 +28,7 @@ namespace Testing
             Assert.Equal("https://paychex-story-point-estimator.azurewebsites.net/CreateRoom", driver.Url);
 
             //Attempt an empty submission, which should fail
-            //Confirm failure by asserting that the browser did not redirect
+            //Confirm failure by asserting that the browser did not redirect, and is still on create room page
             driver.FindElement(By.TagName("button")).Click();
             Thread.Sleep(500);
             Assert.Equal("https://paychex-story-point-estimator.azurewebsites.net/CreateRoom", driver.Url);
@@ -51,20 +53,51 @@ namespace Testing
             Thread.Sleep(500);
 
             //Attempting valid submission and asserting navigation
-            driver.FindElements(By.ClassName("join-create-text-fields"))[0].SendKeys(Keys.Clear + "UserA");
-            Thread.Sleep(500);
-            driver.FindElements(By.ClassName("join-create-text-fields"))[1].SendKeys(Keys.Clear + "TestingRoom");
+            driver.FindElements(By.ClassName("join-create-text-fields"))[1].SendKeys(Keys.Backspace + "TestingRoom");
             driver.FindElement(By.ClassName("mud-checkbox")).Click();
             driver.FindElement(By.TagName("button")).Click();
             Thread.Sleep(500);
-            //Assert.Contains("https://paychex-story-point-estimator.azurewebsites.net/Room", driver.Url);
+            Assert.NotEqual("https://paychex-story-point-estimator.azurewebsites.net/CreateRoom", driver.Url);
             Thread.Sleep(500);
+
+            userB();
 
             driver.FindElements(By.ClassName("admin-tools-buttons"))[4].Click();
             Thread.Sleep(1000);
             driver.FindElement(By.TagName("html")).SendKeys(Keys.Tab + Keys.Enter);
-            Thread.Sleep(5000);
+            Thread.Sleep(3000);
             driver.Close();
+        }
+
+        public void userB()
+        {
+            var driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://paychex-story-point-estimator.azurewebsites.net/");
+            
+            driver.FindElement(By.LinkText("Join Room")).Click();
+            Thread.Sleep(500);
+
+            //Empty submission
+            driver.FindElements(By.TagName("button"))[1].Click();
+            
+            //Filling in username
+            Thread.Sleep(200);
+            driver.FindElement(By.TagName("input")).SendKeys("UserB");
+            Thread.Sleep(500);
+            
+            //Submission with only username
+            driver.FindElements(By.TagName("button"))[1].Click();
+            
+            //Locating dropdown and selecting the room that was just created by User A
+            var select = new SelectElement(driver.FindElement(By.TagName("select")));
+            select.SelectByText("TestingRoom");
+            Thread.Sleep(500);
+
+            //Valid submission, confirming redirection
+            driver.FindElements(By.TagName("button"))[1].Click();
+            Thread.Sleep(200);
+            Assert.NotEqual("https://paychex-story-point-estimator.azurewebsites.net/JoinRoom", driver.Url);
         }
     }
 }
